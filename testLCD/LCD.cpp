d5d6b6f05553cb122630a7ea7b287ae848b43b56
ItsRayK <xboxer221@live.com>
2016-04-13 11:42:12 -0400

    Please help and fix, MEOW!!!!!!!!

---
+++ testLCD/LCD.cpp
@@ -0,0 +1,26 @@
+#include "LCD.h"
+
+LCD::LCD(int p1, int p2, int p3, int p4, int p5, int p6):lcd(p1, p2, p3, p4, p5, p6){
+  
+}
+
+void LCD::displayRobotLocation(float x, float y){
+  lcd.setCursor(0,0);
+  lcd.print("X:");
+  lcd.print(x);
+  lcd.print(" Y:");
+  lcd.print(y);
+
+}
+
+void LCD::displayFireLocation(float x, float y, float z){
+  lcd.setCursor(0,0);
+  lcd.print("X:");
+  lcd.print(x);
+  lcd.print(" Y:");
+  lcd.print(y);
+  lcd.setCursor(0,1);
+  lcd.print("Z:");
+  lcd.print(z);
+}
+
---
+++ testLCD/LCD.h
@@ -0,0 +1,16 @@
+#ifndef LCD_H
+#define LCD_H
+#include <LiquidCrystal.h>
+
+
+class LCD{
+  private:
+          
+
+  public:
+         void displayRobotLocation(float x, float y);
+         void displayFireLocation(float x, float y, float z);
+          
+};
+
+#endif
