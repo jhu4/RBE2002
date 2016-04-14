cc3115a0b1db3c54f853033eb8ad6c3d492074a6
ItsRayK <xboxer221@live.com>
2016-04-14 17:10:55 -0400

    iahdfoadsfjc

---
+++ testFanController/FanController.cpp
@@ -0,0 +1,29 @@
+#include "FanController.h"
+
+FanController::FanController(Servo _yaw, Servo _pitch):
+  yaw(_yaw),
+  pitch(_pitch){
+    
+  }
+
+void FanController::initialize(){
+  
+}
+
+void FanController::up(){
+  
+}
+
+void FanController::down(){
+  
+}
+
+void FanController::left(){
+  
+}
+
+void FanController::right(){
+  
+}
+
+
--- testFanController/FanController.h
+++ testFanController/FanController.h
@@ -1,11 +1,26 @@
 #ifndef FANCONTROLLER_H
 #define FANCONTROLLER_H
 #include <Servo.h>
+#include "LightSensor.h"
 
 class FanController{
   private:
+    Servo yaw;
+    Servo pitch;
+    LightSensor lightsensor;
+    int reportPitchAngle();
+    int reportYawAngle();
+    float getCandleDistance();
+    boolean findCandle();
     
   public:
-  
+    FanController(Servo yaw, Servo pitch);
+    
+    void initialize();
+    void up();
+    void down();
+    void left();
+    void right();
 };
 
+#endif
