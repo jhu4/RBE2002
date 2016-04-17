c9afa724d7d5d1d0c7e54d2e94d4014677cc1a0d
Dorothy <jhu4@wpi.edu>
2016-04-17 18:59:44 -0400

    fixed format

--- tests/RBE2002_library/Motor.h
+++ tests/RBE2002_library/Motor.h
@@ -1,21 +1,13 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 
-#define FORWARD 1
-#define BACKWARD 0
-
 class Motor{
 	private:
-		int speedPin;
-		int dirAPin;
-		int dirBPin;
-		bool dir;
-		int speed;
+		int pin;
+		int directionpin;
 		
 	public:	
-		Motor(int speedPin, int dirAPin, int dirBPin);
-		setDir(bool forwardDir);
-		setSpeed(int speed);
+		Motor(int p, int directionpin);
 		~Motor();
 };
 
