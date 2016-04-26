d31b6963c7d7f9bde876c6da3de6f75be774f027
Dorothy <jhu4@wpi.edu>
2016-04-26 01:02:02 -0400

    FEEL FREE TO KILL ZACH

--- src/testLightSensor.cpp
+++ src/testLightSensor.cpp
@@ -1,2 +1,13 @@
-// #include <Arduino.h>
-// #include "LightSensor.h"
+#include <Arduino.h>
+#include "LightSensor.h"
+
+LightSensor ls(3,150);
+void setup(){
+  Serial.begin(9600);
+  ls.initialize();
+}
+
+void loop(){
+  ls.sense();
+  Serial.println(ls.getAverageReading());
+}
